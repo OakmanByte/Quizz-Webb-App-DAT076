bbfc169637a65695463a253ea88203c7d5c0771f
OakmanByte <antonskebaber@gmail.com>
2021-02-10 14:21:42 +0100

    Composite key class

---
+++ src/main/java/com/quizzapp/dat076/model/database/dao/key/CompositeKeys.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.quizzapp.dat076.model.database.dao.key;
+
+/**
+ *
+ * @author anton
+ */
+public class CompositeKeys {
+    
+
+// Question is a waek entitety , uniquliy identifeied by its primary key and quizz id 
+
+}
+
+
