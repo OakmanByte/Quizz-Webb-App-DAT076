3b3eefea5a96022365ea556bfd5e7f9c4d5afa6f
Emma <emmadirnberger@gmail.com>
2021-02-11 11:37:02 +0100

    Refactoring in Account and persistence.xml

--- src/main/java/com/quizapp/dat076/model/dao/AccountDAO.java
+++ src/main/java/com/quizapp/dat076/model/dao/AccountDAO.java
@@ -9,6 +9,7 @@ import com.mysema.query.jpa.impl.JPAQuery;
 import com.mysema.query.jpa.impl.JPAUpdateClause;
 import com.quizapp.dat076.model.entity.Account;
 import com.quizapp.dat076.model.entity.QAccount;
+import com.quizapp.dat076.model.entity.Quiz;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -24,24 +25,27 @@ public class AccountDAO extends AbstractDAO<String, Account> {
     @Getter
     @PersistenceContext(unitName = "QuizApp")
     private EntityManager entityManager;
-    private QAccount account = QAccount.account;
+    private final QAccount account = QAccount.account;
 
     public AccountDAO() {
         super(Account.class);
-        //query = new JPAQuery(entityManager);
     }
 
-    public Account findByEmail(String email) {
+    public Account findAccountByEmail(String email) {
         JPAQuery query = new JPAQuery(entityManager);
         return query.from(QAccount.account)
                 .where(QAccount.account.email.eq(email))
                 .singleResult(account);
     }
 
-    public Account findByUsername(String username) {
+    public Account findAccountByUsername(String username) {
         JPAQuery query = new JPAQuery(entityManager);
         return query.from(QAccount.account)
                 .where(QAccount.account.username.eq(username))
                 .singleResult(account);
+    }
+    
+    public void getQuiz(Quiz quiz){
+        //TODO
     }
 }
