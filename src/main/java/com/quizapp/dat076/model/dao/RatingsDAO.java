b25077d2c6fcd2caf1b171837ab2a404653704d3
Anton Blomdell <anton0blomdell@gmail.com>
2021-02-23 14:47:33 +0100

    Ratings Commiit

--- src/main/java/com/quizapp/dat076/model/dao/RatingsDAO.java
+++ src/main/java/com/quizapp/dat076/model/dao/RatingsDAO.java
@@ -6,14 +6,16 @@
 package com.quizapp.dat076.model.dao;
 
 import com.mysema.query.jpa.impl.JPAQuery;
-import com.quizapp.dat076.model.entity.Category;
-import com.quizapp.dat076.model.entity.QCategory;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import com.quizapp.dat076.model.entity.QRatings;
 import com.quizapp.dat076.model.entity.Ratings;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import javax.annotation.PostConstruct;
+import javax.persistence.criteria.CriteriaQuery;
 import lombok.Getter;
 
 /**
@@ -29,11 +31,48 @@ public class RatingsDAO extends AbstractDAO<String,Ratings> {
     private EntityManager entityManager;
     private JPAQuery query;
     private QRatings rating = QRatings.ratings;
+      List<Ratings> ratings;
 
+ public List<Ratings> getRatigns() {
+        return new ArrayList<>(ratings);
+    }
+
+  
     public RatingsDAO() {
            super(Ratings.class);
             query = new JPAQuery(entityManager);
     }
+    
+    public List<Ratings> getRatings(int size) {
+
+        if (size > ratings.size()) {
+            Random rand = new Random();
+
+            List<Ratings> randomList = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                int randomIndex = rand.nextInt(ratings.size());
+                randomList.add(ratings.get(randomIndex));
+            }
+
+            return randomList;
+        }
+
+        else {
+            return new ArrayList<>(ratings.subList(0, size));
+        }
+    }
+
+    
+    public void addRating(Ratings rating){
+        entityManager.persist(rating);
+    }
+    
+    public List<Ratings> findAllRatings(){
+        CriteriaQuery<Ratings> cq = entityManager.getCriteriaBuilder().createQuery(Ratings.class);
+        cq.select(cq.from(Ratings.class));
+        return entityManager.createQuery(cq).getResultList();
+    }
+  
     
   public Ratings FindHighRated(int score)
    {
