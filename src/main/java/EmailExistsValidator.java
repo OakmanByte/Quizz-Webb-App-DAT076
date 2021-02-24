9e07988daec62a96c87fa24c4f6e4e29a6e4e5d0
Koppar95 <axelborn@student.chalmers.se>
2021-02-24 11:21:14 +0100

    new files for email validation (lab help)

---
+++ src/main/java/EmailExistsValidator.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Rebec
+ */
+import com.quizapp.dat076.model.dao.AccountDAO;
+import static javax.validation.constraintvalidation.ValidationTarget.ANNOTATED_ELEMENT;
+import static javax.validation.constraintvalidation.ValidationTarget.PARAMETERS;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.validator.ValidatorException;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import javax.validation.constraintvalidation.SupportedValidationTarget;
+
+@SupportedValidationTarget({ANNOTATED_ELEMENT, PARAMETERS})
+public class EmailExistsValidator implements ConstraintValidator<EmailExists, String> {
+
+    @EJB
+    private AccountDAO accountDAO;
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext context) {
+
+        return accountDAO.findAccountByEmail(email) != null;
+
+    }
+}
