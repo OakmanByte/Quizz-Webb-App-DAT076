bc49f5807d993524a243c685cefe5acec11dd9a5
Koppar95 <axelborn@student.chalmers.se>
2021-02-24 11:19:01 +0100

    moved sort funcion here from controller

--- src/main/java/com/quizapp/dat076/model/beans/LeaderboardBackingBean.java
+++ src/main/java/com/quizapp/dat076/model/beans/LeaderboardBackingBean.java
@@ -3,20 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.quizapp.dat076.model.beans;
 
 import com.quizapp.dat076.controller.LeaderboardController;
+import com.quizapp.dat076.model.dao.LeaderboardDAO;
 import com.quizapp.dat076.model.entity.Account;
 import com.quizapp.dat076.model.entity.Leaderboard;
 import java.io.Serializable;
 import java.util.List;
 import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import javax.inject.Inject;
 import lombok.Data;
-
 
 /**
  *
@@ -25,18 +25,33 @@ import lombok.Data;
 @Data
 @Named
 @ViewScoped
-public class LeaderboardBackingBean implements Serializable{
-    private List<Leaderboard> accounts;
-    
+public class LeaderboardBackingBean implements Serializable {
+
+    private List<Leaderboard> board;
+
+    @EJB
+    private LeaderboardDAO leaderboardDAO;
+
     @Inject
     private LeaderboardController controller;
-    
+
     @PostConstruct
-    public void init(){
-       accounts = controller.getAccounts();
+    public void init() {
+        board = leaderboardDAO.findScoresByQuizID(controller.getQuiz1().getId());
+        //board = controller.getAccounts(board,8);
     }
-    
-    public int test (){
-        return accounts.size();
+
+    public List<Leaderboard> getBoard() {
+        board.sort((l1, l2)
+                -> l2.getScore().compareTo(
+                        l1.getScore()));
+        return board;
+    }
+
+    public void test() {
+        System.out.println("BIITCH \n");
+        for (Leaderboard x : board) {
+            System.out.printf("username: %s , quizid: %d , score: %d \n", x.getAccount().getUsername(), x.getQuiz().getId(), x.getScore());
+        }
     }
 }
