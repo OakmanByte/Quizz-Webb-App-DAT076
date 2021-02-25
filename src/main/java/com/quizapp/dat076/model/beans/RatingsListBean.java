189b65a5d0993589429651c341b534db6610f734
Anton Blomdell <anton0blomdell@gmail.com>
2021-02-25 15:15:13 +0100

    Ratings Change, implement template and remove unnessary things

--- src/main/java/com/quizapp/dat076/model/beans/RatingsListBean.java
+++ src/main/java/com/quizapp/dat076/model/beans/RatingsListBean.java
@@ -5,12 +5,20 @@
  */
 package com.quizapp.dat076.model.beans;
 
+import com.quizapp.dat076.controller.RatingsController;
 import com.quizapp.dat076.model.dao.RatingsDAO;
 import com.quizapp.dat076.model.entity.Ratings;
+import homepageBeans.LoginViewBean;
+import java.io.Serializable;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.util.List;
+import java.util.UUID;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import org.primefaces.PrimeFaces;
 
 
 /**
@@ -18,19 +26,76 @@ import java.util.List;
  * @author Antpack
  */
 @Named
-public class RatingsListBean {
+@ViewScoped
+public class RatingsListBean implements Serializable {
+    
+      private List<Ratings> allRatings;
+      private Ratings selectedRating;
+      private List<Ratings> selectedRatings;
     
     @Inject
     private RatingsDAO ratingsDAO;
-    private List<Ratings> allRatings;
+ 
+  //  private RatingsController ratingsController;
     
     @PostConstruct
     public void postConstruct(){
-        allRatings = ratingsDAO.findAllRatings();
+        allRatings = ratingsDAO.findAll();
+        
     }
     
-    public List<Ratings> getAllRatings(){
+    public List<Ratings> getRatings(){
         return allRatings;
     }
+       public Ratings getSelectedRatings() {
+        return selectedRating;
+    }
+
+    public void setSelectedProduct(Ratings selectedRating) {
+        this.selectedRating = selectedRating;
+    }
+
+    public List<Ratings> getSelectedProducts() {
+        return selectedRatings;
+    }
+
+    public void setSelectedProducts(List<Ratings> selectedRatings) {
+        this.selectedRatings = selectedRatings;
+    }
+
+    public void openNew() {
+        this.selectedRating = new Ratings();
+    }
+    
+    
+
+    public void deleteRating() {
+        this.allRatings.remove(this.selectedRating);
+        this.selectedRating = null;
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Product Removed"));
+        PrimeFaces.current().ajax().update("form:messages", "form:dt-ratings");
+    }
+
+    public String getDeleteButtonMessage() {
+        if (hasSelectedRatings()) {
+            int size = this.selectedRatings.size();
+            return size > 1 ? size + " ratings selected" : "1 ratings selected";
+        }
+
+        return "Delete";
+    }
+
+    public boolean hasSelectedRatings() {
+        return this.selectedRatings != null && !this.selectedRatings.isEmpty();
+    }
+
+    public void deleteSelectedProducts() {
+        this.allRatings.removeAll(this.selectedRatings);
+        this.selectedRatings = null;
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Ratings Removed"));
+        PrimeFaces.current().ajax().update("form:messages", "form:dt-ratings");
+        PrimeFaces.current().executeScript("PF('dtratings').clearFilters()");
+    }
+    
     
 }
