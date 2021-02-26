8870a09b3f26c6eb01d72923a756ef2e94ba8560
Albin1118 <31730135+Albin1118@users.noreply.github.com>
2021-02-26 10:11:05 +0100

    Fix error in QuizDAOTest by adding Question to Shrinkwrap

--- src/test/java/com/quizapp/dat076/model/dao/QuizDAOTest.java
+++ src/test/java/com/quizapp/dat076/model/dao/QuizDAOTest.java
@@ -7,6 +7,7 @@ package com.quizapp.dat076.model.dao;
 
 import com.quizapp.dat076.model.entity.Account;
 import com.quizapp.dat076.model.entity.Category;
+import com.quizapp.dat076.model.entity.Question;
 import com.quizapp.dat076.model.entity.Quiz;
 import java.util.List;
 import javax.ejb.EJB;
@@ -47,7 +48,7 @@ public class QuizDAOTest {
     @Deployment
     public static WebArchive createDeployment() {
         return ShrinkWrap.create(WebArchive.class)
-                .addClasses(QuizDAO.class, Quiz.class, AccountDAO.class, Account.class, CategoryDAO.class, Category.class)
+                .addClasses(QuizDAO.class, Quiz.class, AccountDAO.class, Account.class, CategoryDAO.class, Category.class, Question.class)
                 .addAsResource("META-INF/persistence.xml")
                 .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
     }
