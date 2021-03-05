aa548e30e2e8ed8601fbd9906be9b1f9ef922c2f
OakmanByte <antonskebaber@gmail.com>
2021-03-05 20:11:47 +0100

    Starting to implement password hashing with argon2 algorithm

---
+++ src/main/java/com/quizapp/dat076/model/Argon2PasswordEncoder.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.quizapp.dat076.model;
+
+/**
+ *
+ * @author Anton Ekman
+ */
+public class Argon2PasswordEncoder {
+    
+    
+    
+    
+    
+}
