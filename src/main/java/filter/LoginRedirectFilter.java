b5582c0a16f588f9899c83c42b76e407b96d0180
OakmanByte <antonskebaber@gmail.com>
2021-03-11 15:40:31 +0100

    Cleaning up

--- src/main/java/filter/LoginRedirectFilter.java
+++ src/main/java/filter/LoginRedirectFilter.java
@@ -20,7 +20,7 @@ import javax.servlet.http.HttpServletResponse;
  * A filter class that handles filteration of diffrent pages based on the users roll. Makes it possible to restric certain pages from none users or none admins. 
  * @author Anton Ekman
  */
-@WebFilter({"/temp.xhtml", "/accountpage.xhtml", "/quizpage.xhtml"})
+@WebFilter({"/temp.xhtml", "/accountpage.xhtml"})
 
 public class LoginRedirectFilter extends HttpFilter {
 
