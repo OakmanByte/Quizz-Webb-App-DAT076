fe6f8c53c557eb3706998f58f850dbbad56000f3
Emma <emmadirnberger@gmail.com>
2021-03-12 12:02:17 +0100

    Create quiz now works with Category!

--- src/main/java/com/quizapp/dat076/model/beans/CreateQuizBackingBean.java
+++ src/main/java/com/quizapp/dat076/model/beans/CreateQuizBackingBean.java
@@ -5,6 +5,7 @@
  */
 package com.quizapp.dat076.model.beans;
 
+import com.quizapp.dat076.model.dao.CategoryDAO;
 import com.quizapp.dat076.model.dao.QuizDAO;
 import com.quizapp.dat076.model.entity.Category;
 import com.quizapp.dat076.model.entity.Question;
@@ -16,6 +17,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.validation.constraints.NotNull;
 import lombok.Data;
+import org.primefaces.event.SelectEvent;
 
 /**
  *
@@ -25,6 +27,7 @@ import lombok.Data;
 @Named("createQuiz")
 @ViewScoped
 public class CreateQuizBackingBean implements Serializable {
+
     //Attributes
     @NotNull
     private String title;
@@ -35,21 +38,28 @@ public class CreateQuizBackingBean implements Serializable {
     @EJB
     private QuizDAO quizDAO;
     
+    @EJB
+    private CategoryDAO catDAO;
+
     @Inject
     private UserBean userBean;
 
     public void addQuiz() {
         //Correct answer = alt 1 always atm
         Question newQuestion = new Question();
-        //Quiz first = new Quiz("First quiz", account1, c1);
-       // Category cat = new Category(category.toString());
-        Quiz quizToCreate = new Quiz(title, userBean.getAccount(), null);
-        quizToCreate.setQuizzCategory(category);
-        quizDAO.update(quizToCreate);
+        Quiz quizToCreate = new Quiz(title, userBean.getAccount(), category);
+//        quizToCreate.setQuizzCategory(category);
+//        quizDAO.update(quizToCreate);
         System.out.println("***** " + quizToCreate.getCreator().getUsername() + " " + category);
-        
-        
+
         quizDAO.create(quizToCreate);
         System.out.println("***** Quiz created is " + quizDAO.findQuizByID(quizToCreate.getId()));
+    }
+
+    public void onItemSelectedListener(SelectEvent event) {
+        String catTitle = (String) event.getObject();
+        Category selectedItem = catDAO.find(catTitle);
+        System.out.println("**** Category is maybe: " + selectedItem);
+        category = selectedItem;
     }
 }
