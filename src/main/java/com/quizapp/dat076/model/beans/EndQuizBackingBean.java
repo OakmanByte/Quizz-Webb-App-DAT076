30cd04f1a3a0847f9c006367be01189e6e327d9c
Albin1118 <31730135+Albin1118@users.noreply.github.com>
2021-03-15 15:22:24 +0100

    Improve formatting and information shown on endquiz.xhtml

--- src/main/java/com/quizapp/dat076/model/beans/EndQuizBackingBean.java
+++ src/main/java/com/quizapp/dat076/model/beans/EndQuizBackingBean.java
@@ -9,13 +9,12 @@ package com.quizapp.dat076.model.beans;
  *
  * @author Anton
  */
-
-
-
+import com.quizapp.dat076.model.dao.QuestionDAO;
 import com.quizapp.dat076.model.dao.QuizDAO;
 import com.quizapp.dat076.model.dao.RatingsDAO;
 import com.quizapp.dat076.model.entity.Quiz;
 import java.io.Serializable;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
@@ -26,40 +25,57 @@ import org.omnifaces.cdi.Param;
 @Data
 @Named
 @ViewScoped
-public class EndQuizBackingBean implements Serializable{
-    
+public class EndQuizBackingBean implements Serializable {
+
     @Inject
     @Param(name = "quizId")
     private int quizId;
+    
     @Inject
     @Param(name = "points")
     private int points;
-    
+
     private Quiz quiz;
+    private int maxPoints;
+    private String message;
+
     @EJB
     private QuizDAO quizDAO;
     @EJB
+    private QuestionDAO questionDAO;
+    @EJB
     private RatingsDAO ratingsDAO;
-    
-     @Inject
+
+    @Inject
     private UserBean userBean;
-     
+
     private boolean disableEndquiz;
-   
-    public Quiz fetchQuiz(){
-     
-        quiz = quizDAO.find(quizId);
-        return quiz;
-    }
     
-    public boolean disableButtons(){
-        if(userBean.getAccount().getUsername() == null){
-        disableEndquiz = true;
+    /**
+     * Initialize the bean by retrieving the quiz
+     **/
+    @PostConstruct
+    private void init() {
+        
+        quiz = quizDAO.find(quizId);
+        
+        maxPoints = questionDAO.findQuestionsinQuiz(quiz).size() * 10;
+        
+        if(points == 0){
+            message = "Zero points, just like your IQ!";
+        }else{
+            message = "Well done!";
         }
-        else{
-        disableEndquiz=false;
-        }
-      return disableEndquiz;
+        
     }
-  
+
+    public boolean disableButtons() {
+        if (userBean.getAccount().getUsername() == null) {
+            disableEndquiz = true;
+        } else {
+            disableEndquiz = false;
+        }
+        return disableEndquiz;
+    }
+
 }
